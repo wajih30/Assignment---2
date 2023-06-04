bb842eb778965c64f52936bb04d86421b91c2200
wajih30 <129690707+wajih30@users.noreply.github.com>
2023-06-04 23:19:04 +0500

    Update complexnumber.cpp

--- complexnumber.cpp
+++ complexnumber.cpp
@@ -2,10 +2,36 @@
 #inlcude "complex.h"
 
 using namespace std;
-IntComplex::IntComplex(int number,int complex) : (real),imaginary(imaginary){}
-IntComplex IntComplex::operator+(const IntComplex& sum)const{
-          int r1 = real + sum.real;
-          int i1=imaginary + add.imaginary;
+intcomplex::intcomplex(int number,int complex) : number(number),complex(complex){}
+intcomplex intcomplex::operator+(const intcomplex& addition)const{
+          int n1 = number + addition.number;
+          int c1=complex + addition.complex;
+          return intcomplex(n1,c1);
+}
+intcomplex intcomplex::operator-(const intcomplex& asubtraction)const{
+          int n1 = number + subtraction.number;
+          int c1=complex + subtraction.complex;
+          return intcomplex(n1,c1);
+}
+intcomplex intcomplex::operator*(const intcomplex& multiplication)const{
+          int n1 = number + multiplication.number;
+          int c1=complex + multiplication.complex;
+          return intcomplex(n1,c1);
+}
+intcomplex intcomplex::operator/(const intcomplex& division)const{
+          int n1 = number + division.number;
+          int c1=complex + division.complex;
+          return intcomplex(n1,c1);
+}
+int intcomplex::getnumber()const{
+          return number;
+}
+int intcomplex::getcomplex()const{
+          return complex;
+}
+
+
+
   
  
   
