318f32e6387bcfe4d4037d17288d912554b5c1b9
wajih30 <129690707+wajih30@users.noreply.github.com>
2023-06-04 19:35:18 +0500

    Update complexnumber.cpp

--- complexnumber.cpp
+++ complexnumber.cpp
@@ -1,1 +1,13 @@
+#include <iostream>
+#inlcude "complex.h"
+
+using namespace std;
+IntComplex::IntComplex(int number,int complex) : (real),imaginary(imaginary){}
+IntComplex IntComplex::operator+(const IntComplex& sum)const{
+          int r1 = real + sum.real;
+          int i1=imaginary + add.imaginary;
+  
+ 
+  
+
 
