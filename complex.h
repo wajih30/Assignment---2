327298b5dec7891b157f6314628cd275a02b426c
wajih30 <129690707+wajih30@users.noreply.github.com>
2023-06-04 23:00:47 +0500

    Update complex.h

--- complex.h
+++ complex.h
@@ -10,6 +10,12 @@ public:
   intcomplex(int number = 0, int complex =0) : number(number) , complex(complex){}
 int getnumber()const;
 int getcomplex()const;
+intcomplex operator+(const intcomplex& addition)const;
+intcomplex operator-(const intcomplex& subtraction)const;
+intcomplex operator*(const intcomplex& multiplication)const;
+intcomplex operator/(const intcomplex& division)const;
+};
+  
 
 
 
