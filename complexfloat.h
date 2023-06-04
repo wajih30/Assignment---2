b7120a86a52c0e03d17d522aadd2685ca98bd159
wajih30 <129690707+wajih30@users.noreply.github.com>
2023-06-04 23:29:14 +0500

    Update complexfloat.h

--- complexfloat.h
+++ complexfloat.h
@@ -1,1 +1,19 @@
+#pragma once
+#define complexfloat
+#include <iostream>
+#include "Complex.h"
+using namespace std;
 
+class complexfloat:public intcomplex{
+private:
+  float number;
+  float complex;
+public:
+  complexfloat(float number=0.0f,int complex =0.0f);
+  float getnumber()const;
+  float getnumber()const;
+  complexfloat operator+(const complexfloat& addition) const;
+  complexfloat operator-(const complexfloat& subtraction) const;
+  complexfloat operator*(const complexfloat& multiplication) const;
+  complexfloat operator/(const complexfloat& division) const;
+}
