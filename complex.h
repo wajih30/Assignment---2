f5c347c85ee28cef7e82182c0f64557c871d4b8d
wajih30 <129690707+wajih30@users.noreply.github.com>
2023-06-04 19:45:23 +0500

    Update complex.h
    creating header

--- complex.h
+++ complex.h
@@ -1,2 +1,17 @@
 #pragma once
+#define intcomplex
+
+
+class intcomplex{
+private:
+  int number;
+  int complex
+public:
+  intcomplex(int number = 0, int complex =0) : number(number) , complex(complex){}
+int getnumber()const;
+int getcomplex()const;
+
+
+
+
 
