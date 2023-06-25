6e1cd07a6ad59ec62fe1a6223db628db5f7ccb20
wajih30 <129690707+wajih30@users.noreply.github.com>
2023-06-25 15:25:58 +0500

    Update complexnumber.cpp
    mistakenly added the wrong operators for subtraction,multiplication and division

--- complexnumber.cpp
+++ complexnumber.cpp
@@ -9,18 +9,18 @@ intcomplex intcomplex::operator+(const intcomplex& addition)const{
           return intcomplex(n1,c1);
 }
 intcomplex intcomplex::operator-(const intcomplex& asubtraction)const{
-          int n1 = number + subtraction.number;
-          int c1=complex + subtraction.complex;
+          int n1 = number - subtraction.number;
+          int c1=complex - subtraction.complex;
           return intcomplex(n1,c1);
 }
 intcomplex intcomplex::operator*(const intcomplex& multiplication)const{
-          int n1 = number + multiplication.number;
-          int c1=complex + multiplication.complex;
+          int n1 = number *multiplication.number;
+          int c1=complex* multiplication.complex;
           return intcomplex(n1,c1);
 }
 intcomplex intcomplex::operator/(const intcomplex& division)const{
-          int n1 = number + division.number;
-          int c1=complex + division.complex;
+          int n1 = number /division.number;
+          int c1=complex / division.complex;
           return intcomplex(n1,c1);
 }
 int intcomplex::getnumber()const{
