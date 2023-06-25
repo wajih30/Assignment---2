23f838626daccdf4e72956fd027ae705101a5626
wajih30 <129690707+wajih30@users.noreply.github.com>
2023-06-25 15:28:34 +0500

    Update complexnumber2.cpp
    mistakenly added the wrong operators while operator overloading

--- complexnumber2.cpp
+++ complexnumber2.cpp
@@ -8,18 +8,18 @@ complexfloat complexfloat:: operator+(const complexfloat& addition)const{
   return complexfloat(n1,c1);
 }
 complexfloat complexfloat:: operator-(const complexfloat& subtraction)const{
-  float n1=number+ subtraction .number;
-  float c1=complex+ subtraction .complex;
+  float n1=number-subtraction .number;
+  float c1=complex- subtraction .complex;
   return complexfloat(n1,c1);
 }
 complexfloat complexfloat:: operator*(const complexfloat& multiplication )const{
-  float n1=number+ multiplication.number;
-  float c1=complex+ multiplication.complex;
+  float n1=number* multiplication.number;
+  float c1=complex* multiplication.complex;
   return complexfloat(n1,c1);
 }
 complexfloat complexfloat:: operator/(const complexfloat& division)const{
-  float n1=number+ division.number;
-  float c1=complex+ division.complex;
+  float n1=number/ division.number;
+  float c1=complex/ division.complex;
   return complexfloat(n1,c1);
 }
 int complexfloat::getnumber()const {
